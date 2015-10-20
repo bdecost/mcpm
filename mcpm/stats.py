c8544f28b7a9b3c88d02acc8db2fee1deb04a1e9
Brian DeCost <bdecost@andrew.cmu.edu>
2015-10-20 16:37:59 -0400

    replaced pandas stats format with plain hdf5 format that I'm currently using with spparks.

--- mcpm/stats.py
+++ mcpm/stats.py
@@ -1,49 +1,42 @@
 """ microstructure data output """
+import h5py
 import numpy as np
-import pandas as pd
 
 from . import io
 
 stats_df = None  # pandas dataframe for stats output
-state_ids = None # list of Potts states
+unique_ids = None # list of Potts states
 output_index = 0
 stats_file = None
 
 def initialize(sites, args):
-  global stats_df
-  global state_ids
+  global unique_ids
   global stats_file
-  num_snapshots = 1 + (args.length / args.freq)
-  state_ids = np.sort(np.unique(sites))
-  zero_data = np.zeros((num_snapshots,state_ids.size+1), dtype=float)
-  columns = ['time'] + list(map(str, state_ids))
-  stats_df = pd.DataFrame(zero_data,
-                          columns=columns,
-                          index=np.arange(0,num_snapshots))
   stats_file = args.statsfile
-  with pd.HDFStore(stats_file) as store:
-    store['stats'] = stats_df
+
+  num_snapshots = 1 + (args.length / args.freq)
+  unique_ids = np.sort(np.unique(sites))
+  np.insert(unique_ids, 0, 0) # insert zero to match index with grain id
+  zero_data = np.zeros((num_snapshots,unique_ids.size), dtype=float)
+  with h5py.File(stats_file) as f:
+    f['time'] = np.zeros(num_snapshots)
+    f['grainsize'] = zero_data
   return
   
 def compute(sites, time=None):
-  global stats_df
   global output_index
+  global unique_ids
+  global stats_file
   # get grain size in voxels
-  stats_dict = {str(state): np.sum(sites == state) for state in state_ids}
-  stats_dict['time'] = time
-  stats_df.loc[output_index] = pd.Series(stats_dict)
+  grainsize = np.array([np.sum(sites == state) for state in unique_ids])
+  with h5py.File(stats_file) as f:
+    f['time'][output_index] = time
+    f['grainsize'][output_index] = grainsize
+
   print('computed stats!')
-  checkpoint()
   output_index += 1
   return
 
-def checkpoint():
-  global stats_df
-  with pd.HDFStore(stats_file) as store:
-    del store['stats']
-    store['stats'] = stats_df
-  return
-
 def finalize():
-  checkpoint()
+  pass
   return
